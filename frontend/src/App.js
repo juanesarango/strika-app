457562ed4acf1f249ed285f39950484354126c1c
Juan Esteban Arango <juanes.ao@gmail.com>
2021-04-26 02:48:41 -0400

    🏠 improve landing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -27,7 +27,7 @@ class App extends React.Component {
             <LeaderBoard user={this.state.user} />
           </div>
         ) : (
-          <div className="bg-gray-50">
+          <div className="bg-gray-50 mt-64">
             <div className="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8 lg:flex lg:items-center lg:justify-between">
               <h2 className="text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl">
                 <span className="block">Ready for the Challenge?</span>
